4cf03b52a80bb1f0025bb30083e9c388af45f157
MikeG <mikegdking@gmail.com>
2019-12-26 16:14:01 -0500

    Everythig is an Object in Python. Mutable and Inmutable

---
+++ m01_02_basic.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+balance = 1000.00
+name = "Mario Gomez"
+account_no = "01123581231"
+
+print("name:", name, "account:", account_no, "balance:", balance)
+print("name:", name, "account:", account_no, "balance:", "$"\ #This (\) is to continue with the code
++ str(balance))                                               #Instead of having a long line of code  
+print("name:", name, "account:", account_no, "balance:", "$" \
++ str(float(balance)))
+
+print("\n")
+
+balance = 1000.00
+new_balance = balance
+balance += 250
+print("balance object id:    ", id(balance), "balance value: ", balance)
+print("new balance object id:", id(balance), "new_balance value:", new_balance)
