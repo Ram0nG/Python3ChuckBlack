40f8c7df53011414d61954e4e2a51bd6f5de9681
MikeG <mikegdking@gmail.com>
2019-12-26 15:55:48 -0500

    String,variables, Numbers & Ducks!

--- m01_01_basic.py
+++ m01_01_basic.py
@@ -4,8 +4,9 @@ balance = 1000
 name = "Mario Gomez"
 account_no = "01123581231"
 
-print("name:", name, "  account:", account_no, "  balance:", balance)
-print("name:", name, "  account:", account_no, "  balance:", "$" + str(balance))
-print("name:", name, "  account:", account_no, "  balance:", "$" + str(float(balance)))
+print("name:", name, "account:", account_no, "balance:", balance)
+print("name:", name, "account:", account_no, "balance:", "$" + str(balance))
+print("name:", name, "account:", account_no, "balance:", "$" \
++ str(float(balance)))
 
 print("\n")
