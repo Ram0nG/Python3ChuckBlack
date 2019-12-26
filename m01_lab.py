d28c777882270bd5974c2e9ae60fab4562b651ef
MikeG <mikegdking@gmail.com>
2019-12-26 16:14:51 -0500

    Complete the Challenge lab?

---
+++ m01_lab.py
@@ -0,0 +1,11 @@
+# Modele 1: LAB
+# Create variable for person, like in Social Media, who has attribute/
+#such as:
+# *name
+# *Living Location
+# *Relationship Status
+# *Employer
+# *Job Title
+#Yearly Income
+
+#Print the Information for this individual Nicely aligned:
