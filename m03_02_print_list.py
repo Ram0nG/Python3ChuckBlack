4259778c57b54b18dd1488ea11982d6b948f04d5
MikeG <mikegdking@gmail.com>
2020-03-24 14:09:59 -0400

    more commends

--- m03_02_print_list.py
+++ m03_02_print_list.py
@@ -4,7 +4,8 @@ account_no = "01123581321"
 
 print("\nName:", name,  "   Account:", account_no, "    original balance:", "$" + str(balance))
 
-charges_file = open("m00_charges-file")
+charges_file = open("m00_charges-file") # Chuck called this the old school way of open a file
 for charge_info_string in charges_file:
-    charge_info = charge_info_string.strip() .split(',')
-    print(charge_info)
+    charge_info = charge_info_string.strip() .split(',') # the .strip off or remove the space format of the file
+    print(charge_info)                     # from the begining and the end of the file.
+                                       # and the .split is a comma separated value explited into a list
