ada0705370e72f1ed83e8ffa4279c5bc3f04d651
MikeG <mikegdking@gmail.comgit config --global user.email mikegdking@gmail.comgit config --global user.email mikegdking@gmail.com>
2020-03-24 19:59:13 -0400

    Data Struct List

--- m04_01_datastruct_list.py
+++ m04_01_datastruct_list.py
@@ -4,19 +4,19 @@ account_no = "01123581221"
 
 print("name:", name, "      account:", account_no, "    original balance:", "$" + str(balance))
 
-charge_list = []
+charges_list = []
 for charge_string in open ("m00_charges-file"): # Everyline in the file is going to be called charge_string
     charge_info = charge_string.strip() .split(",") # This line is going to format the lines in the file striping the space and adding space everyting separated by comma
-    charge_list.append(charge_info) # This will be at list of list
+    charges_list.append(charge_info) # This will be at list of list
 
 print("\n")
 print("Vendor                    Date                charge             Balance")
 print("-------------------      ---------------     -----------------   --------")
-for charge_info in charge_list:
+for charge_info in charges_list:
     balance =  balance - float(charge_info[2])
     print("{0:24}    {1:10}    {2:8,.2f}   {3:20,.2f}".format(charge_info[0],
-                                                             charge_info[1],
-                                                             float(charge_info[2]),
-                                                             balance))
+                                                              charge_info[1],
+                                                              float(charge_info[2]),
+                                                              balance))
 # This is a list of list, this is no the best way to do code
 # in the next example he will show how to do the best practice..
